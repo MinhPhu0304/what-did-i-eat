e8253957e5785b08801c1d756edfe8a6e85ed5c2
minh <Lminhphu0304@gmail.com>
2020-04-20 21:27:54 +1200

    Add get chart data config for bar chart

---
+++ src/utils.js
@@ -0,0 +1,26 @@
+// ts-check
+
+/**
+ * Return data object for chart js to render
+ * @param {object} productData
+ * @param {string} dataLabel
+ */
+export function getChartDataLabel (productData, dataLabel) {
+  const labels = Object.keys(productData)
+  const data = Object.values(productData)
+
+  return {
+    chartData: {
+      labels,
+      datasets: [{
+        data,
+        label: dataLabel,
+        backgroundColor: '#c6e48b'
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }
+}
