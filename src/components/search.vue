72cd399ecb5bf7eb1fa93d4072996b7579e85030
minh <Lminhphu0304@gmail.com>
2020-04-20 17:58:20 +1200

    Add search product by text input

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <video
-      id="video"
-      width="300"
-      height="200"
-      ref="scanner">
-    </video>
-    <button v-on:click="stopRecord"> Stop record </button>
-    <button v-on:click="start"> Start recording </button>
+    <div class="video__scanner__container">
+      <video id="video" width="300" height="200" ref="scanner"></video>
+      <button v-on:click="stopRecord">Stop record</button>
+      <button v-on:click="start">Start recording</button>
+    </div>
+    <div>
+      <form v-on:submit="onTextInputSubmit">
+        <label> Barcode number: <input type="number" v-model="barcodeText"/> </label>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -18,12 +20,12 @@ export default {
   name: 'Search',
   data: function () {
     return {
-      ingredients: '',
       barCodeReader: new BrowserMultiFormatReader(),
       isMediaStreamAPISupported:
         navigator &&
         navigator.mediaDevices &&
-        'enumerateDevices' in navigator.mediaDevices
+        'enumerateDevices' in navigator.mediaDevices,
+      barcodeText: ''
     }
   },
   mounted () {
@@ -34,16 +36,23 @@ export default {
   },
   methods: {
     start () {
-      this.barCodeReader.decodeOnceFromVideoDevice(undefined, 'video').then((result) => {
-        if (result) {
-          this.$emit('onsubmitBarcode', result.text)
-        }
-      }).catch((err) => {
-        console.error(err)
-      })
+      this.barCodeReader
+        .decodeOnceFromVideoDevice(undefined, 'video')
+        .then(result => {
+          if (result) {
+            this.$emit('onsubmitBarcode', result.text)
+          }
+        })
+        .catch(err => {
+          console.error(err)
+        })
     },
     stopRecord () {
       this.barCodeReader.reset()
+    },
+    onTextInputSubmit (e) {
+      e.preventDefault()
+      this.$emit('onsubmitBarcode', this.barcodeText)
     }
   }
 }
@@ -51,4 +60,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.video__scanner__container {
+  display: flex;
+  flex-direction: column;
+}
+.video__scanner__container button{
+  width: 100px;
+}
 </style>
